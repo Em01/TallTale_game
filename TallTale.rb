1a9d93a2fbda4c7e5fddea60fdd55518d0ffa892
Em01 <emma_williams1@hotmail.co.uk>
2014-12-18 20:22:31 +0000

    initial methods created

--- TallTale.rb
+++ TallTale.rb
@@ -1,3 +1,26 @@
 #Script Name: Tall Tale
 #Version: 1.0
 #Description: A ruby script to demonstrate how to collect and process user input through an interactive story game. 
+
+class Screen
+
+	def cls
+		puts("\n" * 25)
+		puts "\a"
+	end
+end
+
+class Tale
+#properties that represent pieces of input which will be received by the player
+
+	attr_accessor :monster, :villain, :object, :place, :location
+
+#what will be used to store the paragraphs that make up the story
+
+	attr_accessor :P1, :P2, :P3, :P4
+	
+#method which displays any text that its passed to it as an argument
+	def tell_Story(paragraph)
+		puts paragraph
+	end
+end
